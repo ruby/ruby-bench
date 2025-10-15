1eca4f1eb8003d48ff24f56ae31056dd5f95a344
Matt Valentine-House <matt@eightbitraptor.com>
2025-10-15 13:46:57 +0100

    Run Ractor tests in CI
    Also make sure that the benchmarks list test is up to date

--- test/benchmarks_test.rb
+++ test/benchmarks_test.rb
@@ -2,12 +2,18 @@ require_relative 'test_helper'
 require 'yaml'
 
 describe 'benchmarks.yml' do
-  it 'has the same entries as /benchmarks' do
+  it 'lists all available benchmarks' do
     yjit_bench = File.expand_path('..', __dir__)
     benchmarks_yml = YAML.load_file("#{yjit_bench}/benchmarks.yml")
+
     benchmarks = Dir.glob("#{yjit_bench}/benchmarks/*").map do |entry|
       File.basename(entry).delete_suffix('.rb')
-    end
-    assert_equal benchmarks.sort, benchmarks_yml.keys.sort
+    end.compact
+
+    benchmarks += Dir.glob("#{yjit_bench}/benchmarks-ractor/*").map do |entry|
+      File.basename(entry).delete_suffix('.rb')
+    end.compact
+
+    assert_equal benchmarks.sort, benchmarks_yml.keys.map{ |k| k.gsub('ractor/', '') }.sort
   end
 end
