ccdb04c336c34b8569f1881a8fecc0b1f21740ff
Luke Gruber <luke.gruber@shopify.com>
2025-10-15 17:59:24 -0400

    Remove useless files in benchmarks-ractor/optcarrot

--- benchmarks-ractor/optcarrot/benchmark.rb
+++ benchmarks-ractor/optcarrot/benchmark.rb
@@ -2,6 +2,7 @@ require_relative '../../harness/loader'
 require_relative "lib/optcarrot"
 
 ROM_PATH = File.join(__dir__, "examples/Lan_Master.nes").freeze
+ENV["WARMUP_ITRS"] = "1"
 
 run_benchmark(10) do
   nes = Optcarrot::NES.new(["-b", "--no-print-video-checksum", ROM_PATH])
