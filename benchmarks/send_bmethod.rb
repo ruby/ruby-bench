b0bb57e8bea30b3961c35a492132be4a608258c1
Max Bernstein <ruby@bernsteinbear.com>
2025-10-20 09:28:53 -0400

    Add bmethod benchmark

---
+++ benchmarks/send_bmethod.rb
@@ -0,0 +1,11 @@
+require_relative '../harness/loader'
+
+define_method(:zero) { :b }
+define_method(:one) { |arg| arg }
+
+run_benchmark(500) do
+  2_000_000.times do |i|
+    zero
+    one 123
+  end
+end
