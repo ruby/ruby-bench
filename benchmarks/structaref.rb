7d9ead5791234ab4618b32d5d71d4e37e7459c28
Max Bernstein <ruby@bernsteinbear.com>
2025-10-30 16:35:44 -0400

    Add struct aref benchmark

---
+++ benchmarks/structaref.rb
@@ -0,0 +1,31 @@
+require_relative '../harness/loader'
+
+TheClass = Struct.new(:v0, :v1, :v2, :levar)
+
+def get_value_loop obj
+  sum = 0
+  # 1M
+  i = 0
+  while i < 1000000
+    # 10 times to de-emphasize loop overhead
+    sum += obj.levar
+    sum += obj.levar
+    sum += obj.levar
+    sum += obj.levar
+    sum += obj.levar
+    sum += obj.levar
+    sum += obj.levar
+    sum += obj.levar
+    sum += obj.levar
+    sum += obj.levar
+    i += 1
+  end
+
+  return sum
+end
+
+obj = TheClass.new(1, 2, 3, 1)
+
+run_benchmark(850) do
+  get_value_loop obj
+end
