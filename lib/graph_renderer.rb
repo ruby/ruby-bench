7c3492487ca5fab7e923acf4475dd761a89a6b38
Rafael Mendonça França <rafael.franca@shopify.com>
2025-11-18 17:24:49 +0000

    Extract default configuration for the graph

--- lib/graph_renderer.rb
+++ lib/graph_renderer.rb
@@ -12,6 +12,17 @@ end
 
 # Renders benchmark data as a graph
 class GraphRenderer
+  DEFAULT_WIDTH = 1600
+  COLOR_PALETTE = %w[#3285e1 #489d32 #e2c13e #8A6EAF #D1695E].freeze
+  THEME = {
+    colors: COLOR_PALETTE,
+    marker_color: '#dddddd',
+    font_color: 'black',
+    background_colors: 'white'
+  }.freeze
+  DEFAULT_BOTTOM_MARGIN = 30.0
+  DEFAULT_LEGEND_MARGIN = 4.0
+
   def self.render(json_path, png_path, title_font_size: 16.0, legend_font_size: 12.0, marker_font_size: 10.0)
     json = JSON.load_file(json_path)
     ruby_descriptions = json.fetch("metadata")
@@ -20,19 +31,14 @@ class GraphRenderer
     bench_names = data.first.last.keys
 
     # ruby_descriptions, bench_names, table
-    g = Gruff::Bar.new(1600)
+    g = Gruff::Bar.new(DEFAULT_WIDTH)
     g.title = "Speedup ratio relative to #{ruby_descriptions.keys.first}"
     g.title_font_size = title_font_size
-    g.theme = {
-      colors: %w[#3285e1 #489d32 #e2c13e #8A6EAF #D1695E],
-      marker_color: '#dddddd',
-      font_color: 'black',
-      background_colors: 'white'
-    }
+    g.theme = THEME
     g.labels = bench_names.map.with_index { |bench, index| [index, bench] }.to_h
     g.show_labels_for_bar_values = true
-    g.bottom_margin = 30.0
-    g.legend_margin = 4.0
+    g.bottom_margin = DEFAULT_BOTTOM_MARGIN
+    g.legend_margin = DEFAULT_LEGEND_MARGIN
     g.legend_font_size = legend_font_size
     g.marker_font_size = marker_font_size
 
