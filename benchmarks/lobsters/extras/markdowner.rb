709dbd9caa7bb06bdaea2c12582c559f544bf70c
John Hawthorn <john@hawthorn.email>
2026-01-19 22:52:11 -0800

    Revert "Update commonmarker"
    This reverts commit e04769206f3e49633a3609a4e4ec6c0dfaf28179.

--- benchmarks/lobsters/extras/markdowner.rb
+++ benchmarks/lobsters/extras/markdowner.rb
@@ -1,23 +1,17 @@
 class Markdowner
   # opts[:allow_images] allows <img> tags
 
-  COMMONMARKER_OPTIONS = {
-    parse: { smart: true },
-    render: { unsafe: true },
-    extension: { tagfilter: true, autolink: true, strikethrough: true }
-  }.freeze
-
   def self.to_html(text, opts = {})
     if text.blank?
       return ""
     end
 
-    # Preprocess @mentions before markdown parsing
-    processed_text = preprocess_mentions(text.to_s)
+    exts = [:tagfilter, :autolink, :strikethrough]
+    root = CommonMarker.render_doc(text.to_s, [:SMART], exts)
 
-    html = Commonmarker.to_html(processed_text, options: COMMONMARKER_OPTIONS)
+    walk_text_nodes(root) {|n| postprocess_text_node(n) }
 
-    ng = Nokogiri::HTML(html)
+    ng = Nokogiri::HTML(root.to_html([:DEFAULT], exts))
 
     # change <h1>, <h2>, etc. headings to just bold tags
     ng.css("h1, h2, h3, h4, h5, h6").each do |h|
@@ -39,13 +33,44 @@ class Markdowner
     end
   end
 
-  def self.preprocess_mentions(text)
-    text.gsub(/\B(@#{User::VALID_USERNAME})/) do |match|
-      user = match[1..-1]
-      if User.exists?(username: user)
-        "[#{match}](#{Rails.application.root_url}u/#{user})"
+  def self.walk_text_nodes(node, &block)
+    return if node.type == :link
+    return block.call(node) if node.type == :text
+
+    node.each do |child|
+      walk_text_nodes(child, &block)
+    end
+  end
+
+  def self.postprocess_text_node(node)
+    while node
+      return unless node.string_content =~ /\B(@#{User::VALID_USERNAME})/
+      before, user, after = $`, $1, $'
+
+      node.string_content = before
+
+      if User.exists?(:username => user[1..-1])
+        link = CommonMarker::Node.new(:link)
+        link.url = Rails.application.root_url + "u/#{user[1..-1]}"
+        node.insert_after(link)
+
+        link_text = CommonMarker::Node.new(:text)
+        link_text.string_content = user
+        link.append_child(link_text)
+
+        node = link
       else
-        match
+        node.string_content += user
+      end
+
+      if after.length > 0
+        remainder = CommonMarker::Node.new(:text)
+        remainder.string_content = after
+        node.insert_after(remainder)
+
+        node = remainder
+      else
+        node = nil
       end
     end
   end
