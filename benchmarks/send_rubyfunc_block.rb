e4c0c3ae1c8a819abd817d80edd18d53d888cb40
Takashi Kokubun <takashikkbn@gmail.com>
2026-04-03 10:22:30 -0700

    Suppress a warning from send_rubyfunc_block

--- benchmarks/send_rubyfunc_block.rb
+++ benchmarks/send_rubyfunc_block.rb
@@ -1,8 +1,9 @@
 require_relative '../harness/loader'
 
 class C
-  def ruby_func
+  def ruby_func(call_block)
     # Don't even yield
+    yield if call_block
   end
 end
 
@@ -11,14 +12,14 @@ INSTANCE = C.new
 run_benchmark(500) do
   500_000.times do |i|
     # Manually unrolling to avoid loop overhead
-    INSTANCE.ruby_func {}
-    INSTANCE.ruby_func {}
-    INSTANCE.ruby_func {}
-    INSTANCE.ruby_func {}
-    INSTANCE.ruby_func {}
-    INSTANCE.ruby_func {}
-    INSTANCE.ruby_func {}
-    INSTANCE.ruby_func {}
-    INSTANCE.ruby_func {}
+    INSTANCE.ruby_func(false) {}
+    INSTANCE.ruby_func(false) {}
+    INSTANCE.ruby_func(false) {}
+    INSTANCE.ruby_func(false) {}
+    INSTANCE.ruby_func(false) {}
+    INSTANCE.ruby_func(false) {}
+    INSTANCE.ruby_func(false) {}
+    INSTANCE.ruby_func(false) {}
+    INSTANCE.ruby_func(false) {}
   end
 end
