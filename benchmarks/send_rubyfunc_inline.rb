d771f81f1ce9db51376e03ca7b8e6a83160556d4
Takashi Kokubun <takashikkbn@gmail.com>
2026-07-08 09:17:51 -0700

    Add send_rubyfunc_inline benchmark (#522)

---
+++ benchmarks/send_rubyfunc_inline.rb
@@ -0,0 +1,32 @@
+require_relative '../harness/loader'
+
+INNER_ITERATIONS = 10_000_000
+EXPECTED_RESULT = 2 * INNER_ITERATIONS * INNER_ITERATIONS + 4 * INNER_ITERATIONS
+
+def send_rubyfunc_inline_callee(a, b, c, d)
+  a + b + c + d
+end
+
+def send_rubyfunc_inline_driver(limit)
+  total = 0
+  i = 0
+
+  while i < limit
+    total += send_rubyfunc_inline_callee(i, i + 1, i + 2, i + 3)
+    i += 1
+  end
+
+  total
+end
+
+@send_rubyfunc_inline_result = 0
+
+100.times do
+  @send_rubyfunc_inline_result = send_rubyfunc_inline_driver(100)
+end
+
+run_benchmark(20) do
+  @send_rubyfunc_inline_result = send_rubyfunc_inline_driver(INNER_ITERATIONS)
+end
+
+raise "unexpected result: #{@send_rubyfunc_inline_result}" unless @send_rubyfunc_inline_result == EXPECTED_RESULT
