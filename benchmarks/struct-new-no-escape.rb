951cc3d5c08ad7b867af69911adb3029f015f1ca
Max Bernstein <max@bernsteinbear.com>
2026-09-15 17:57:03 -0400

    Add struct new micro benchmark (#536)
    Benchmark allocating, writing, and reading fields

---
+++ benchmarks/struct-new-no-escape.rb
@@ -0,0 +1,16 @@
+require_relative '../harness/loader'
+
+Point = Struct.new(:x, :y)
+
+def test
+  p = Point.new(1, 2)
+  p.x + p.y
+end
+
+run_benchmark(100) do
+  i = 0
+  while i < 1_000_000
+    test
+    i += 1
+  end
+end
